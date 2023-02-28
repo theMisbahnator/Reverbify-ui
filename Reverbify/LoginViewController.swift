be12e40d3fea50e3255966814ce54cdf603f697f
Misbah Imtiaz <misb2001@gmail.com>
2023-02-28 02:46:05 -0600

    added the loading page with a pulse animation, transitions into a login view controller

---
+++ Reverbify/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Reverbify
+//
+//  Created by Misbah Imtiaz on 2/28/23.
+//
+
+import UIKit
+
+class LoginController : UIViewController {
+    
+    @IBOutlet weak var userName: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Reverbify/ViewController.swift
+++ Reverbify/ViewController.swift
@@ -8,13 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("First Commit!")
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // starts animation with half second delay
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+            self.animate()
+        })
+        
+        // stops the animation after 4 seconds, goes to next screen
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
+            // This should either be the login page, or
+            // if that user is signed in, the home page
+                
+            // Present the next view controller
+            let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "login") as! LoginController
+            loginVC.modalTransitionStyle = .crossDissolve
+            loginVC.modalPresentationStyle = .fullScreen
+            self.present(loginVC, animated: true)
+        }
+    }
+    
+    private func animate() {
+        // pulse animation of logo
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: [.autoreverse, .repeat], animations: {
+            self.imageView.transform = CGAffineTransform(scaleX: 1.15, y: 1.15)
+        }, completion: nil)
+    }
 
 }
 
