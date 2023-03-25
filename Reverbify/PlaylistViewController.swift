da0c4814a07335fe5926a11da05163eca316d637
Pawan K Somavarpu <pawanksomavarpu@utexas.edu>
2023-03-25 17:55:22 -0500

    working all playlist page

---
+++ Reverbify/PlaylistViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PlaylistViewController.swift
+//  Reverbify
+//
+//  Created by Pawan K Somavarpu on 3/25/23.
+//
+
+import UIKit
+
+class PlaylistViewController: UIViewController {
+    
+    var playlist : Playlist? = nil
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Reverbify/ReverbifyAPIHandler.swift
+++ Reverbify/ReverbifyAPIHandler.swift
@@ -31,6 +31,22 @@ class Song {
     }
 }
 
+class Playlist {
+    var title : String
+    var thumbnail: String
+    var songs: Array<Song>
+    
+    init(title: String, thumbnail: String, songs: Array<Song>) {
+        self.title = title
+        self.thumbnail = thumbnail
+        self.songs = songs
+    }
+
+    func toString() -> String {
+        return "\n\(title)\n\(thumbnail)\n\(songs)"
+    }
+}
+
 class ReverbifyAPIHandler {
     var userName: String
     var view : UIViewController
