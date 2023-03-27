80dada538a39ef8b067d085715867fb16749e417
pawansomavarpu <77944873+pawansomavarpu@users.noreply.github.com>
2023-03-27 00:28:32 -0500

    added playlist table view cell

---
+++ Reverbify/PlaylistTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  SongTableCell.swift
+//  Reverbify
+//
+//  Created by Misbah Imtiaz on 3/19/23.
+//
+
+import UIKit
+
+class PlaylistTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet var thumbnail: UIImageView!
+    @IBOutlet var title: UILabel!
+    @IBOutlet var numberSongs: UILabel!
+    
+    //    @IBOutlet weak var title: UILabel!
+//    @IBOutlet weak var author: UILabel!
+//    @IBOutlet weak var timestamp: UILabel!
+//    @IBOutlet weak var duration: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
